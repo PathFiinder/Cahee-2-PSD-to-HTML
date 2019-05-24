1eccb75c47f49c2f03bf5b258380f8c0a9474208
Krystian Konecki <krystian_konecki@wp.pl>
2019-05-24 15:26:04 +0200

    Changing RWD - Mobile First

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+//Navigation - burger
+
+document.querySelector('i.fa-bars').addEventListener('click', function () {
+    document.querySelector('nav').classList.toggle('active');
+    document.querySelector('.ul-list').classList.toggle('active');
+})
+
+
 //SCROLL
 const headerPosTop = $('header').offset().top
 const aboutUsPosTop = $('.about-us').offset().top
@@ -9,43 +17,56 @@ const contactPosTop = $('.contact').offset().top
 const footerPosTop = $('footer').offset().top
 
 const navHeight = $('nav').height()
-
-function toScroll(PosTop, navH) {
-    $('body, html').animate({
-        scrollTop: PosTop - navH
-    }, 1000)
-}
-
-$('.a-home').on('click', function () {
-    toScroll(headerPosTop, 0)
-})
-
-$('.a-about').on('click', function () {
-    toScroll(aboutUsPosTop, navHeight)
-})
-
-$('.a-services').on('click', function () {
-    toScroll(ourServicesPosTop, navHeight)
-})
-
-$('.a-gallery').on('click', function () {
-    toScroll(galleryPosTop, navHeight)
-})
-
-$('.a-blog').on('click', function () {
-    toScroll(blogPosTop, navHeight)
-})
-
-$('.a-contact').on('click', function () {
-    toScroll(contactPosTop, navHeight)
-})
+let activeH = 0
 
 //FLEX
 
-const classLista = ['section.gallery','div.wrap-gallery','div.wrap-content-3','div.wrap-content-4'];
+const classLista = ['section.gallery', 'div.wrap-gallery', 'div.wrap-content-3', 'div.wrap-content-4'];
 
 document.querySelector('.view-more').addEventListener('click', function () {
-    for(let i=0;i<classLista.length;i++){
+    for (let i = 0; i < classLista.length; i++) {
         document.querySelector(classLista[i]).classList.toggle("active");
     }
+    if (document.querySelector('.wrap-gallery').classList.contains('active')) {
+        document.querySelector('.view-more').textContent = "view less"
+        activeH = 458
+
+    } else {
+        document.querySelector('.view-more').textContent = "view more"
+        activeH = 0
+    }
+
+})
+
+//SCROLL
+function toScroll(PosTop, navH,activeH) {
+    $('body, html').animate({
+        scrollTop: PosTop - navH + activeH
+    }, 1000)
+    document.querySelector('nav').classList.remove('active');
+    document.querySelector('.ul-list').classList.remove('active');
+}
+
+$('.a-home').on('click', function () {
+    toScroll(headerPosTop, 0,activeH)
+})
+
+$('.a-about').on('click', function () {
+    toScroll(aboutUsPosTop, navHeight,activeH)
+})
+
+$('.a-services').on('click', function () {
+    toScroll(ourServicesPosTop, navHeight,activeH)
+})
+
+$('.a-gallery').on('click', function () {
+    toScroll(galleryPosTop, navHeight,activeH)
+})
+
+$('.a-blog').on('click', function () {
+    toScroll(blogPosTop, navHeight,activeH)
+})
+
+$('.a-contact').on('click', function () {
+    toScroll(contactPosTop, navHeight,activeH)
 })
