e5f5cb446178037086e97e69038aaa4e3c2bff15
Krystian Konecki <krystian_konecki@wp.pl>
2019-05-27 15:34:12 +0200

    Fixing scroll bug

--- js/script.js
+++ js/script.js
@@ -7,17 +7,18 @@ document.querySelector('i.fa-bars').addEventListener('click', function () {
 
 
 //SCROLL
+
 const headerPosTop = $('header').offset().top
 const aboutUsPosTop = $('.about-us').offset().top
 const ourServicesPosTop = $('.our-services').offset().top
 const galleryPosTop = $('.gallery').offset().top
 const subscribeUsPosTop = $('.subscribe-us').offset().top
-const blogPosTop = $('.blog').offset().top
+let blogPosTop = $('.blog').offset().top
 const contactPosTop = $('.contact').offset().top
 const footerPosTop = $('footer').offset().top
 
 const navHeight = $('nav').height()
-let activeH = 0
+
 
 //FLEX
 
@@ -27,45 +28,45 @@ document.querySelector('.view-more').addEventListener('click', function () {
     for (let i = 0; i < classLista.length; i++) {
         document.querySelector(classLista[i]).classList.toggle("active");
     }
+    blogPosTop = $('.blog').offset().top
     if (document.querySelector('.wrap-gallery').classList.contains('active')) {
         document.querySelector('.view-more').textContent = "view less"
-        activeH = 458
+
     } else {
         document.querySelector('.view-more').textContent = "view more"
-        activeH = 0
     }
-    activeH = 0
+
 })
 
 //SCROLL
-function toScroll(PosTop, navH, activeH) {
+function toScroll(PosTop, navH) {
     $('body, html').animate({
-        scrollTop: PosTop - navH - activeH
+        scrollTop: PosTop - navH
     }, 1000)
     document.querySelector('nav').classList.remove('active');
     document.querySelector('.ul-list').classList.remove('active');
 }
 
 $('.a-home').on('click', function () {
-    toScroll(headerPosTop, 0, activeH)
+    toScroll(headerPosTop, 0)
 })
 
 $('.a-about').on('click', function () {
-    toScroll(aboutUsPosTop, navHeight, activeH)
+    toScroll(aboutUsPosTop, navHeight)
 })
 
 $('.a-services').on('click', function () {
-    toScroll(ourServicesPosTop, navHeight, activeH)
+    toScroll(ourServicesPosTop, navHeight)
 })
 
 $('.a-gallery').on('click', function () {
-    toScroll(galleryPosTop, navHeight, activeH)
+    toScroll(galleryPosTop, navHeight)
 })
 
 $('.a-blog').on('click', function () {
-    toScroll(blogPosTop, navHeight, activeH)
+    toScroll(blogPosTop, navHeight)
 })
 
 $('.a-contact').on('click', function () {
-    toScroll(contactPosTop, navHeight, activeH)
+    toScroll(contactPosTop, navHeight)
 })
